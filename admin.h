01289ec89a85d54848ec602122bfdc6b62afece8
vk-eveth <138586570+vk-eveth@users.noreply.github.com>
2024-02-12 22:43:37 +0300

    Create admin.h

---
+++ admin.h
@@ -0,0 +1,13 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+void admin_menu();
+int authenticate();
+void add_patient();
+void add_doctor();
+void view_patient_doctor_list();
+void delete_patient();
+void delete_doctor();
+void display_admin_menu();
+
+#endif
